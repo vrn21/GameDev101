0da82b58dccb61a50d4e61f737e7211ca056fb4b
vrn21 <22203171-vrn21@users.noreply.replit.com>
2023-06-10 15:39:41 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame, sys
+from pygame.locals import QUIT
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hello World!')
+
+
+#game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+          running = False
+            
+            sys.exit()
+    pygame.display.update()
