1e6e66e6d15e94631eda47d1963627fb8911e50b
vrn21 <116856685+vrn21@users.noreply.github.com>
2023-06-10 17:35:03 +0000

    UserIcon Added

--- main.py
+++ main.py
@@ -4,16 +4,30 @@ from pygame.locals import QUIT
 #initialisation
 pygame.init()
 #screen
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-DISPLAYCAPTION = pygame.display.set_caption("Space Invaders")
+screen = pygame.display.set_mode((800, 600))
+caption = pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("556435.png")
-DISPLAYICON = pygame.display.set_icon(icon)
+displayicon = pygame.display.set_icon(icon)
+
+#player
+playimg = pygame.image.load("space-invaders.png")
+playerx = 370
+playery = 480
+
+
+def player(x, y):
+    screen.blit(playimg, (x, y))
+
 
 #game loop
 running = True
 while running:
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             running = False
+
+    player(playerx, playery)
 
     pygame.display.update()
