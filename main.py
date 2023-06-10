b42679d0904e7cd21ee26dd3d8a43f8eb9aa61b9
vrn21 <116856685+vrn21@users.noreply.github.com>
2023-06-10 15:49:41 +0000

    Some changes

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import pygame, sys
 from pygame.locals import QUIT
 
+#initialisation
 pygame.init()
+#screen
 DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World!')
-
 
 #game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT():
-          running = False
-            
-            
+            running = False
+
     pygame.display.update()
